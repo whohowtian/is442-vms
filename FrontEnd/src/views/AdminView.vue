eafccf84d87c931c8e1b038c75bece3c5697d676
kwwwk <91783866+kwwwk@users.noreply.github.com>
2023-03-07 05:17:04 +0800

    Updated AdminView page Navbar

--- FrontEnd/src/views/AdminView.vue
+++ FrontEnd/src/views/AdminView.vue
@@ -1,42 +1,67 @@
-<template>
-    <div class="container-fluid">
+<script>
+import NavBar from '../components/Navbar.vue';
 
-    <h1 style="text-align: center; margin-top: 20px;">Login as..</h1>
-    <div class="row mt-4" id="login">
-        <div class="col">
-            <div class="card">
-                <a href="LJhome">
-                    <div class="card-body">
-                        <h2 class="card-title" style="font-size:4vw;">Learner</h2>
-                        <img src="../Icons/learner.png" class="img-fluid" alt="learner">
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <a href="hrHome">
-                    <div class="card-body">
-                        <h2 class="card-title" style="font-size:3vw;" >Human Resource</h2>
-                        <img src="../Icons/hr.png" class="img-fluid" alt="HR">
-                    </div>
-                </a>
-            </div>
+    export default {
+        name: "AdminView",
+        components: {
+            NavBar,
+         },
+        data() {
+            return {
+            menuItems: [
+                { label: 'HOME', route: '/AdminView'  },
+                { label: 'ACCOUNT', route: '/AccountView'  },
+                { label: 'WORKFLOW', route: '/WorkflowView'  },
+                { label: 'LOGOUT', route: '/'  }
+            ]
+            }
+        }
+    };
+</script>
+
+<!-- admin home page -->
+<template >
+    <NavBar :items="menuItems" />
+    <div class="row mt-4 mb-5" id = 'welcome'>
+        <div class="line">
+            <p style = "font-weight:600;">WELCOME, JOHN</p>
         </div>
     </div>
-</div>
+    <div class="row "></div>
 
+    <div class="row mt-5 ">
+        <div class="col-sm-1"></div>
+        <div class="col">
+            <div class="card bg-light h-100">
+                <!-- change the link to the correct page -->
+                <a href="">
+                    <div class="card-body" >
+                        
+                        <img src="../assets/icons/adminEditAccount.png" class="mx-auto d-block img-fluid">
+                        <h4 class="card-title">Edit Account</h4>
+                        <h6 class="card-text">Create/ Read/ Update/ Delete accounts of users</h6>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card bg-light h-100">
+                <!-- change the link to the correct page -->
+                <a href="">
+                    <div class="card-body">
+                        <img src="../assets/icons/adminEditWorkflow.png" class="mx-auto d-block img-fluid">
+                        <h4 class="card-title">Assign & Edit Workflow</h4>
+                        <h6 class="card-text">
+                            <ul>
+                                <li>Assign/ View/ Edit the workflow</li>
+                                <li>Check the workflow status</li>
+                            </ul>
+                        </h6>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-sm-1"></div>
 
+    </div>
 </template>
-
-<style>
-#login {
-  display: flex;
-  align-items: center;
-}
-.card{
-    width: 65%; 
-    margin: auto;
-}
-
-</style>
