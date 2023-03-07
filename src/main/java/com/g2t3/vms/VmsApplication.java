7b1b55b7c81ef118faa5ffdcab1014a531e87987
Veronica Teng <veronicatengyy@gmail.com>
2023-03-08 00:34:56 +0800

    Removed unnecessary annotations from main and added bat files

--- src/main/java/com/g2t3/vms/VmsApplication.java
+++ src/main/java/com/g2t3/vms/VmsApplication.java
@@ -2,12 +2,8 @@ package com.g2t3.vms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@EnableMongoRepositories
 public class VmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(VmsApplication.class, args);
