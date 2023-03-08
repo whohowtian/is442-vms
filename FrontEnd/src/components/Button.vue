f97eff750a4f6ef99fac8552edb62139e85ce3da
kwwwk <91783866+kwwwk@users.noreply.github.com>
2023-03-08 21:23:02 +0800

    button n table reusable component

---
+++ FrontEnd/src/components/Button.vue
@@ -0,0 +1,42 @@
+<template>
+    <button class="my-button" :class="[buttonStyle === 'clear' ? 'clear' : buttonStyle === 'solid' ? 'solid' : 'none']" @click="$emit('click')">
+      <slot></slot>
+    </button>
+</template>
+  
+<script>
+  export default {
+    name: 'Button',
+    props: {
+      buttonStyle: {type: String,default: 'clear'}
+    }
+  }
+</script>
+  
+<style>
+  .my-button {
+    border: none;
+    padding: 0.3rem;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .clear {
+    background-color: #fff;
+    color: #0079B3;
+    border: 2px solid #007bff;
+    font-weight: bold;;
+
+  }
+  
+  .solid {
+    background-color: #fff;
+    color: #007bff;
+    font-weight: bold;
+  }
+  .none {
+    color: #007bff;
+    font-weight: bold;
+  }
+  </style>
+  
---
+++ FrontEnd/src/components/Table.vue
@@ -0,0 +1,53 @@
+<template>
+    <table class="my-table">
+      <thead>
+        <tr>
+          <th v-for="(header, index) in headers" :key="index">{{ header }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in data" :key="index">
+          <td v-for="(field, index) in fields" :key="index">{{ item[field] }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </template>
+  
+<script>
+  export default {
+    props: {
+      data: {
+        type: Array,
+        default: [],
+      },
+      headers: {
+        type: Array,
+        default: [],
+      },
+      fields: {
+        type: Array,
+        default: [],
+      },
+    },
+  };
+</script>
+
+<style scoped>
+.my-table {
+  width: 100%;
+  border-collapse: collapse;
+  font-size: 14px;
+  text-align: left;
+}
+
+.my-table th,
+.my-table td {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+.my-table th {
+  background-color: #f2f2f2;
+  font-weight: bold;
+}
+</style>
