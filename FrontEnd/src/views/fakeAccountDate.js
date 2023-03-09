b5e68426913dddee782233eecc32bb1ef7dc50b5
kwwwk <91783866+kwwwk@users.noreply.github.com>
2023-03-09 09:54:56 +0800

    add account page table with fake data

---
+++ FrontEnd/src/views/fakeAccountDate.js
@@ -0,0 +1,12 @@
+const fakeAccountDate = 
+    [
+      { id: 1, name: 'ahahah', email: "dhcqwno@hsdnfer.com",company: "223avda", userRole:'Approval'},
+      { id: 2, name: 'bds', email: "Aderwno@hsdnfer.com",company: "22vda", userRole:'Vendor'},
+      { id: 3, name: 'bds', email: "wdwebwno@hsdnfer.com",company: "2avda", userRole:'Admin'},
+      { id: 4, name: 'dtbwt', email: "no@hsdnfer.com",company: "2da", userRole:'Approval'},
+      { id: 5, name: 'wqe', email: "cqwno@hsdnfer.com",company: "avda", userRole:'Approval'},
+      { id: 6, name: 'br', email: "dwno@hsdnfer.com",company: "223da", userRole:'Approval'},
+      { id: 7, name: 'wetb', email: "cqwno@hsdnfer.com",company: "2da", userRole:'Approval'},
+    ];
+
+  export default fakeAccountDate;
