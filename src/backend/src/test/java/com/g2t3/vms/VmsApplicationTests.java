03fca3ea4ea623e05cd95902b899300f56f2a533
Veronica Teng <veronicatengyy@gmail.com>
2023-03-14 03:39:07 +0800

    Created build.bat file to run BE/FE

--- src/backend/src/test/java/com/g2t3/vms/VmsApplicationTests.java
+++ src/backend/src/test/java/com/g2t3/vms/VmsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class VmsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
