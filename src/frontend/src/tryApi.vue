0690cda31eb9441c123040941c850c8668c14399
kwwwk <91783866+kwwwk@users.noreply.github.com>
2023-03-19 23:47:27 +0800

    update tryAPI vue

--- src/frontend/src/tryApi.vue
+++ src/frontend/src/tryApi.vue
@@ -1,8 +1,9 @@
 <template>
-    <form @submit.prevent=submitForm()>
+    <!-- change the method here to try GET or POST -->
+    <form @submit.prevent=postData()> 
         <button type="submit" class="btn btn-primary">Try API</button>
         <p>
-            console.log to see if api can be called on web (no CrossOrigin error)
+            open terminal to see if api can be called on web (no CrossOrigin error)
         </p>
             
     </form>
@@ -12,29 +13,29 @@
   import axios from 'axios';
   export default{
     methods:{
-    getData(){
-        const allData = '/email/sendEmail'
+    getData(){ //GET method
+        const allData = 'http://localhost:8080/api/formtemplate'
         axios.get(allData).then(response=>{
           var seeAllData = response.data
           console.log(seeAllData)
         })
       },
-      async submitForm() {
-        console.log("_______________________")
-        const data = {
-            "toEmail": "kaiwei.chua.2020@scis.smu.edu.sg",
-            "body": "TESTING",
-            "subject": "TESTING",
-            "attachment": ""
-            }
-          try {
-          const response = await axios.post('http://localhost:8080/email/sendEmail', data);
-          console.log("SUCCESSFULLY POST")
-          console.log(response.data); // do something with the response data
-        } catch (error) {
-          console.error(error);
-        }
-      }, 
+    async postData() {  //POST method
+      console.log("_______________________")
+      const data = {
+          "toEmail": "kaiwei.chua.2020@scis.smu.edu.sg",
+          "body": "TESTING",
+          "subject": "TESTING",
+          "attachment": ""
+          }
+        try {
+        const response = await axios.post('http://localhost:8080/email/sendEmail', data);
+        console.log("SUCCESSFULLY POST")
+        console.log(response.data); // do something with the response data
+      } catch (error) {
+        console.error(error);
+      }
+    }, 
     }
   }
 </script>
