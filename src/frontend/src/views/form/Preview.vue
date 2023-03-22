7a7a4edecec3c251ae36e96bb5e97d5d07f5a502
kwwwk <91783866+kwwwk@users.noreply.github.com>
2023-03-23 00:46:32 +0800

    POST formTemplate API

--- src/frontend/src/views/form/Preview.vue
+++ src/frontend/src/views/form/Preview.vue
@@ -16,6 +16,9 @@
             </div>
           
         </template>
+        <!-- change to submitForm() to post API , put a unique formNo:"XXX" in "data"
+ -->
+        <button type="button" class="btn btn-primary" @click=getData()>Save</button>
       </el-form>
     </el-main>
   </el-container>
@@ -25,14 +28,81 @@
 <script>
 import {FormBuilder} from './formbuilder'
 import { store } from '../../store.js';
+import axios from 'axios';
 
 export default {
-  name: 'Publish',
   components: FormBuilder.components,
   data(){
     return {
-      forms: store._state.data.forms
+      forms: store._state.data.forms,
+      formNo:"TESTING2",
+      formName:"TESTING",
+      formEffDate:"",
+      AllSections:[], //sectionName , adminUseOnly(default False), doScoreCalculation(default False), 
+      AllQuestion:[]
     }
+  },
+  mounted() {
+    this.formEffDate = this.getToday();
+  },
+  methods: {
+    // calculate today date
+    getToday() {
+      let today = new Date();
+      let year = today.getFullYear();
+      let month = String(today.getMonth() + 1).padStart(2, '0');
+      let day = String(today.getDate()).padStart(2, '0');
+      return `${year}-${month}-${day}`;
+    },
+     // GET METHOD
+    getData(){
+      const formData =this.forms
+      console.log("Data", formData)
+      },
+    // POST METHOD
+    async submitForm() {
+      const formData =this.forms
+
+      //store formSection dict
+      for(let i=0; i<formData.length; i++){
+        let formTitle = formData[i]['title']
+        let allQn = []
+
+        //store questions dict 
+        for (let j =0; j< (formData[i]['fields']).length; j++){
+          let qnDict={
+            qnTitle:formData[i]['fields'][j]['label'],
+            inputType: formData[i]['fields'][j]['fieldType'],
+            isRequired: formData[i]['fields'][j]['isRequired'], //required field
+          };
+          allQn.push(qnDict)
+        }
+
+        this.AllSections.push({
+          sectionName: formTitle,
+          adminUseOnly: false,
+          doScoreCalculation: false,
+          questions:allQn
+          }
+          );
+        }
+      // console.log("OUTPUT", this.AllSections)
+
+        const data = {
+            "formNo": this.formNo,
+            "formName": this.formName,
+            "formEffDate": this.formEffDate,
+            "formSections": this.AllSections,
+        }
+        console.log(data)
+          try {
+          const response = await axios.post('http://localhost:8080/api/formtemplate/create', data);
+          console.log("SUCCESSFULLY POST")
+          console.log(response.data); // do something with the response data
+        } catch (error) {
+          console.error(error);
+        }
+      },
   }
   // computed: {
   //   cssProps() {
