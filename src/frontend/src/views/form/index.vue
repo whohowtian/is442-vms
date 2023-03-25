ad53c8d0fe72a2e905fbf3a5bf176ec041b62705
fangting99 <ftneo.2020@scis.smu.edu.sg>
2023-03-25 12:35:42 +0800

    add navbar to the editable form page

--- src/frontend/src/views/form/index.vue
+++ src/frontend/src/views/form/index.vue
@@ -1,4 +1,5 @@
 <template>
+<NavBar :items="menuItems" />
 <div id="app">
   <el-tabs v-model="activeName">
     <el-tab-pane label="Design" name="Design"><home/></el-tab-pane>
@@ -8,6 +9,7 @@
 </template>
 
 <script>
+import NavBar from '../../components/Navbar.vue';
 import Home from './Home.vue';
 import Preview from './Preview.vue';
 
@@ -15,10 +17,17 @@ export default {
   name: 'App',
   data() {
     return {
-      activeName: "Design"
+      activeName: "Design",
+      menuItems: [
+                { label: 'HOME', route: '/AdminView'  },
+                { label: 'ACCOUNT', route: '/AccountView'  },
+                { label: 'WORKFLOW', route: '/WorkflowView'  },
+                { label: 'LOGOUT', route: '/'  }
+            ]
     }
   },
   components: {
+    NavBar,
     Home,
     Preview
   }
