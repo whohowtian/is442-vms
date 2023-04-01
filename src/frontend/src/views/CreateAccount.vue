30bce1fcb6a13a9f5f1d1522161b2a90715eb035
whohowtian <haotian.hu.2021@scis.smu.edu.sg>
2023-04-01 17:15:50 +0800

    createAcc

--- src/frontend/src/views/CreateAccount.vue
+++ src/frontend/src/views/CreateAccount.vue
@@ -1,4 +1,6 @@
 <script>
+import axios from 'axios';
+import { BASE_URL } from '../api.js';
 import NavBar from '../components/Navbar.vue';
 export default {
         name: "AccountView",
@@ -17,13 +19,49 @@ export default {
                 { label: 'LOGOUT', route: '/'  }
             ],
 
-            usertype: "null"
+            name: null,
+            email: null,
+            number: null,
+            usertype: null,
+            companyName: null,
+            companyUEN: null,
+            business: [null, null],
+            registered: null,
+            gstNum: null,
             }
         },
         methods: {
-        createAcc() {alert('Created!');
-            window.location.href = '/SetUserPassword';
-            }
+        async createAcc() {
+            console.log(this.email),
+            console.log(this.name),
+            console.log(this.number),
+            console.log(this.usertype),
+            console.log(this.companyName),
+            console.log(this.companyUEN),
+            console.log(this.business),
+            console.log(this.registered),
+            console.log(this.gstNum)
+            axios.post(`${BASE_URL}/api/user/create`, {
+                email: this.email,
+                name: this.name,
+                number: this.number,
+                userType: this.usertype,
+                entityUEN: this.companyUEN,
+                entityName: this.companyName,
+                entityActivities: this.business,
+                gstregistered: this.registered,
+                gstRegisteredNo: this.gstNum,
+            })
+            .then(response => {
+                    console.log(response.data);
+                    alert(response.data.message);
+                    window.location.href = 'AccountView';
+                    })
+                    .catch(error => {
+                    console.log(error);
+                    alert(error).data;
+                    });
+            },
         },
         computed: {
             selectedR() {
@@ -43,53 +81,94 @@ export default {
                 style="border-radius: 5px;" onclick="history.back()">Back</button>
             </div>
         </div>
-        <form class="row g-3 p-2">
+        <form class="row g-3 p-2" v-on:submit="createAcc()">
             <div><h1>CREATE USER ACCOUNT</h1></div>
             <div><span style="color:red">* Required</span></div>
 
             <!-- Name -->
             <div class="col-lg-6 col-md-6">
                 <h4><label for="inputName" class="form-label">Full Name <span style="color:red">*</span></label></h4>
-                <textarea  class="form-control" rows="1"></textarea>
+                <textarea  class="form-control" rows="1" v-model="name" required></textarea>
             </div>
 
             <!-- Email -->
             <div class="col-lg-6 col-md-6">
                 <h4><label for="inputEmail" class="form-label">Email <span style="color:red">*</span></label></h4>
-                <textarea   class="form-control"  rows="1" required></textarea>
+                <textarea   class="form-control" rows="1" v-model="email" required></textarea>
             </div>
 
-            <!-- Password -->
+            <!-- Number -->
             <div class="col-lg-6 col-md-6">
-                <h4><label for="inputPassword" class="form-label">Password <span style="color:red">*</span></label></h4>
-                <input type="password" class="form-control" rows="1" required>
+                <h4><label for="inputPNum" class="form-label">Phone Number <span style="color:red">*</span></label></h4>
+                <textarea   class="form-control" rows="1" v-model="number" required></textarea>
             </div>
 
             <!-- Role -->
             <div class="col-lg-6 col-md-6">
                 <h4><label for="inputRole" class="form-label">Role <span style="color:red">*</span></label></h4>
-                <select id="inputRole" class="form-select" v-model="usertype">
+                <select id="inputRole" class="form-select" v-model="usertype" required>
                     <option disabled selected value>Select Role</option>
-                    <option value ="vendor">Vendor</option>
-                    <option value="admin">Admin</option>
-                    <option value="approver">Approver</option>
+                    <option value="ADMIN">Admin</option>
+                    <option value="APPROVER">Approver</option>
+                    <option value ="VENDOR">Vendor</option>
                 </select>
             </div>
             
             <!-- Company name -->
-            <div class="col-lg-6 col-md-6" v-if="usertype =='vendor'">
+            <div class="col-lg-6 col-md-6" v-if="usertype =='VENDOR'">
                 <h4><label for="inputCompanyName" class="form-label">Company Name</label> <span style="color:red">*</span></h4>
-                <textarea class="form-control"   rows = '1'></textarea>
+                <textarea class="form-control"  rows = '1' v-model="companyName" required></textarea>
             </div>
 
-            <div class="col-lg-6 col-md-6" v-if="usertype =='vendor'">
-                <h4><label for="inputCompanyUEN" class="form-label">Company UEN <span style="color:red">*</span></label></h4>
-                <textarea  class="form-control" rows="1"></textarea>
+            <div class="col-lg-6 col-md-6" v-if="usertype =='ADMIN' || usertype =='APPROVER'">
+                <h4><label for="inputCompanyName" class="form-label">Company Name</label> </h4>
+                <textarea class="form-control"   rows = '1' value="Quantum Leap Incorporation" disabled></textarea>
             </div>
+
+            <!-- Company UEN -->
+            <div class="col-lg-6 col-md-6" v-if="usertype =='VENDOR'">
+                <h4><label for="inputCompanyUEN" class="form-label">Company UEN <span style="color:red">*</span></label></h4>
+                <textarea  class="form-control" rows="1" v-model="companyUEN" required></textarea>
+            </div>
+
+            <div class="col-lg-6 col-md-6" v-if="usertype =='ADMIN' || usertype =='APPROVER'">
+                <h4><label for="inputCompanyUEN" class="form-label">Company UEN <span style="color:red">*</span></label></h4>
+                <textarea  class="form-control" rows="1" value="201616155N" disabled></textarea>
+            </div>
+
+            <!-- Nature of business -->
+            <div class="col-lg-6 col-md-6" v-if="usertype =='VENDOR'">
+                <h4><label for="inputBusiness" class="form-label">Nature of Business (Pri)</label> <span style="color:red">*</span></h4>
+                <textarea class="form-control"   rows = '1' v-model="business[0]" required></textarea>
+            </div>   
+
+            <div class="col-lg-6 col-md-6" v-if="usertype =='VENDOR'">
+                <h4><label for="inputBusiness" class="form-label">Nature of Business (Sec)</label></h4>
+                <textarea class="form-control"   rows = '1' v-model="business[1]"></textarea>
+            </div> 
+            
+            <!-- GST Number -->
+            <div class="col-lg-6 col-md-6" v-if="usertype =='VENDOR'">
+                <h4><label for="inputRole" class="form-label">GST Registered <span style="color:red">*</span></label></h4>
+                <select id="inputRole" class="form-select" v-model="registered" required>
+                    <option disabled selected value>Select Yes/No</option>
+                    <option :value="true">Yes</option>
+                    <option :value="false">No</option>
+                </select>
+            </div>
+
+            <div class="col-lg-6 col-md-6" v-if="registered ==true">
+                <h4><label for="inputGSTNum" class="form-label">GST Registered Number</label> <span style="color:red">*</span></h4>
+                <textarea class="form-control"   rows = '1' v-model="gstNum" required></textarea>
+            </div>   
             
             <div class="col">
-                <button type="button" class="btn btn-outline-primary px-4 mt-5 float-end" id="createBtn"
-                style="border-radius: 5px;" @click="createAcc()" >Create</button>
+                <input type="submit" class="btn btn-outline-primary px-4 mt-5 float-end" id="createBtn" value="submit"
+                style="border-radius: 5px;">
+            </div>
+
+            <div>
+                <button @click="test()">test</button>
             </div>
             </form>
     </div>
