e174b73f5fd51eba7ccb2b530297dfc01adabccd
fangting99 <ftneo.2020@scis.smu.edu.sg>
2023-04-01 20:00:44 +0800

    Todo- VendorView

--- src/frontend/src/views/VendorView.vue
+++ src/frontend/src/views/VendorView.vue
@@ -2,6 +2,8 @@
     import NavBar from '../components/Navbar.vue';
     import fakeTaskData from './fakeTaskData';
     import Table from "../components/Table.vue";
+    import { BASE_URL } from '../api.js';
+    import axios from 'axios';
 
 
     export default {
@@ -18,19 +20,85 @@
             ],
             userId :'',
             userEmail:'',
+            userName:'',
+            Todo:[],
             activeOption:'taskTable', //default table displaying
-
-            userName:fakeTaskData.todo[0].task, // to fetch from BE
-            data1:fakeTaskData.todo, 
-            headers1:["Task","Form No.","Stage","Status","Date Assigned","Actions"],
-            fields1:["task","formNo","company","id","dateAssign","Actions"],
+            selectedRows: [], //tick checkbox
+            selectAll: false,
 
             data2:fakeTaskData.completed, 
             headers2:["Task","Form No.","Stage","Status","Date Assigned","Actions"],
             fields2:["task","formNo","company","id","dateAssign","Actions"],
             }
         },
+        created() {
+        //user session
+        const user = JSON.parse(sessionStorage.getItem('user'));
+        this.userId = "6426e823533ce37c3e4ddae3"
+        this.userEmail = "yyteng.2019@gmail.com"
+        // if(user == null){
+        //     //hardcode pass data
+        //     this.userId = "6426e823533ce37c3e4ddae3"
+        //     this.userEmail = "yyteng.2019@gmail.com"
+        // }else{
+        //     this.userId = user.userId
+        //     this.userEmail = user.userEmail
+        //     this.userName = user.name
+        // }
+        console.log("userId-->", this.userId); 
+        console.log("userEmail-->",this.userEmail); 
+        this.getAllFormbyStatus('PENDING_VENDOR')
+    },
         methods: {
+        async getAllFormbyStatus(status){
+            axios.get(`${BASE_URL}/api/form/formstatus/`+status)
+            .then(response => {
+                var AllFormbyStatus = response.data.data;
+                for (const workflow of AllFormbyStatus){
+                    console.log(workflow)
+                    var id = workflow.id
+                    var task = workflow.formContent.formName
+                    var status=workflow.status
+                    var stage= this.addStage(status)[0]
+                    var Mstatus= this.addStage(status)[1]
+                    var formEffDate = new Date(workflow.formEffDate).toLocaleDateString('en-GB')   
+                    var deadline = new Date(workflow.deadline).toLocaleDateString('en-GB')  
+                    var assigned_vendor_email = workflow.assigned_vendor_email
+
+                    console.log('test',assigned_vendor_email,this.userEmail)
+
+                    if(assigned_vendor_email==this.userEmail){
+                        this.Todo.push({id:id,task:task,stage:stage,status:Mstatus,formEffDate:formEffDate,deadline:deadline}) 
+                    }
+
+                }
+                console.log(this.Todo)
+            })
+        },
+        addStage(status){ //add stage, Mstatus according to the status
+            var stage = '';
+            var Mstatus = '';
+            if (status == "PENDING_VENDOR"){
+                stage = 'Vendor'
+                Mstatus = 'Pending'
+            }else if (status == "PENDING_ADMIN"){
+                stage = 'Admin'
+                Mstatus = 'Pending'
+            }else if(status == "PENDING_APPROVAL"){
+                stage = 'Approver'
+                Mstatus = 'Pending'
+            }else if(status == "APPROVED"){
+                stage = 'Completed'
+                Mstatus = 'Approved'
+            }else if(status =='ADMIN_REJECTED'){
+                stage = 'Vendor'
+                Mstatus = 'Admin Rejected'
+            }else if (status == "APPROVER_REJECTED"){
+                stage = 'Vendor'
+                Mstatus = 'Approver Rejected'
+            }
+            return [stage, Mstatus]
+        },
         TaskToDoAction(){
             window.open('https://media.makeameme.org/created/i-pray-to-5bed2f.jpg', '_blank');
         },
@@ -41,21 +109,38 @@
             localStorage.setItem('formNo', [formNo,formId])
             window.location.href = "VendorForm";
         },
+        //table styling  function
+        selectAllRows() {
+            this.selectedRows = this.selectAll ? [...this.allFormData] : [];
         },
-        created() {
-        //user session
-        const user = JSON.parse(sessionStorage.getItem('user'));
-        if(user == null){
-            //hardcode pass data
-            this.userId = "6426e823533ce37c3e4ddae3"
-            this.userEmail = "yyteng.2019@gmail.com"
-        }else{
-            this.userId = user.userId
-            this.userEmail = user.userEmail
+        toggleRowSelection(item, event) {
+            if (event.target.tagName === 'TD') {
+                const index = this.selectedRows.findIndex(selectedRow => selectedRow.id === item.id);
+                if (index === -1) {
+                this.selectedRows.push(item);
+                } else {
+                this.selectedRows.splice(index, 1);
+                }
+            }
+        },
+        isSelected(item) {
+            return this.selectedRows.findIndex(selectedRow => selectedRow.id === item.id) !== -1;
+        },
+        },
+        computed: {
+        allRowsSelected() { //table styling function
+            return this.selectedRows.length === this.allFormData.length;
+        },
+    },
+    watch: { //table styling function
+        selectAll(val) {
+        this.selectedRows = val ? [...this.allFormData] : [];
+        },
+        selectedRows(val) {
+        this.selectAll = val.length === this.allFormData.length;
         }
-        console.log("userId-->", this.userId); 
-        console.log("userEmail-->",this.userEmail); 
-    }
+    },
+        
     };
 </script>
 
@@ -63,7 +148,7 @@
     <NavBar :items="menuItems" />
     <div class="row mt-4 mb-5" id = 'welcome'>
         <div class="line">
-            <p style = "font-weight:600;">WELCOME, {{ userName.toUpperCase() }}</p>
+            <p style = "font-weight:600;">WELCOME, {{ this.userName.toUpperCase() }}</p>
         </div>
     </div>
     
@@ -72,15 +157,43 @@
     <p>Please review the checklist below to complete any assigned tasks.</p>
     <el-tabs v-model="activeOption"  type="border-card">
         <el-tab-pane label="To Do" name="taskTable"  @tab-click="activeOption = 'taskTable'">
-            <template #label>To Do({{ data1.length }})</template>
+            <template #label>To Do({{ this.Todo.length }})</template>
         </el-tab-pane>
         <el-tab-pane label="Completed" name="CompletedtaskTable"  @tab-click="activeOption = 'CompletedtaskTable'">
             <template #label>Completed({{ data2.length }})</template>
         </el-tab-pane>
 
         <!-- 2.1) To-do Table content -->
-        <div v-if="activeOption === 'taskTable'">
+        <div v-if="activeOption === 'taskTable' && Todo.length >0" >
             <table class="my-table">
+            <thead>
+                <tr>
+                    <th class="checkbox-col"><input type="checkbox" v-model="selectAll" @change="selectAllRows"></th>
+                    <th>Task</th>
+                    <th>Stage</th>
+                    <th>Status</th>
+                    <th>Assigned Date</th>
+                    <th>Deadline</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in Todo" :key="item.id" @click="toggleRowSelection(item, $event)" :class="{ 'selected': isSelected(item) }">
+                <td class="checkbox-col"><input type="checkbox" v-model="selectedRows" :value="item" @click.stop></td>
+                <td>{{ item.task }}</td>
+                <td>{{ item.stage }}</td>
+                <td>{{ item.status }}</td>
+                <td>{{ item.formEffDate }}</td>
+                <td>{{ item.deadline }}</td>
+                <td >
+                    <el-icon class="el-input__icon" @click="EditEachForm(item.formNo, item.id)">
+                            <Edit />
+                        </el-icon>
+                </td>
+                </tr>
+            </tbody>
+            </table>
+            <!-- <table class="my-table">
             <thead>
             <tr>
                 <th class="checkbox-col"><input type="checkbox"></th>
@@ -108,8 +221,9 @@
                     </td>
                 </tr>
             </tbody>
-        </table>
+        </table> -->
         </div>
+        <div v-if="activeOption === 'taskTable' && Todo.length ==0">You have no task for now!</div>
 
         <!-- 2.2) Completed Table content -->
         <div v-if="activeOption === 'CompletedtaskTable'">
