8273268e31df08a989fbe37e53ff3fc2c628ec51
kwwwk <91783866+kwwwk@users.noreply.github.com>
2023-04-02 21:18:53 +0800

    update

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -11,11 +11,6 @@ import router from './router'
 import './assets/styles/main.css'
 import { VueDraggableNext } from 'vue-draggable-next'
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faTrash, faEllipsis,faPenSquare,faEye } from '@fortawesome/free-solid-svg-icons'
-import '@fortawesome/fontawesome-free/css/all.css';
-import { far } from "@fortawesome/free-regular-svg-icons";
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 import VueLodash from 'vue-lodash'
